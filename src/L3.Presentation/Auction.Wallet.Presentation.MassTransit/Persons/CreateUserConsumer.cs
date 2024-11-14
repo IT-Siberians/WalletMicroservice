c067f38c1f4e741a1039c0ab2336d1b8a071604a
Mike <m.a.ilnitsky@yandex.ru>
2024-11-14 18:40:46 +0700

    GD-W4 Small fix

--- src/L3.Presentation/Auction.Wallet.Presentation.MassTransit/Persons/CreateUserConsumer.cs
+++ src/L3.Presentation/Auction.Wallet.Presentation.MassTransit/Persons/CreateUserConsumer.cs
@@ -5,6 +5,8 @@ using FluentValidation;
 using MassTransit;
 using Microsoft.Extensions.Logging;
 using Otus.QueueDto.User;
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,18 +25,19 @@ public class CreateUserConsumer(
         var validationResult = validator.Validate(command);
         if (!validationResult.IsValid)
         {
-            logger.LogWarning($"Validate Failed: Username = {command.Username}, Id = {command.Id}");
+            var messages = $"{Environment.NewLine}{string.Join(Environment.NewLine, validationResult.Errors.Select(e => $"- {e.PropertyName}: {e.ErrorMessage}"))}";
+            logger.LogError("Validation messages: {Messages}", messages);
             return;
         }
 
         var answer = await handler.HandleAsync(command, new CancellationToken());
         if (answer is BadAnswer badAnswer)
         {
-            logger.LogError($"Error message: {badAnswer.ErrorMessage}");
+            logger.LogError("Error message: {ErrorMessage}", badAnswer.ErrorMessage);
         }
         else
         {
-            logger.LogInformation($"Created user: Username = {command.Username}, Id = {command.Id}");
+            logger.LogInformation("Created user: Username = {Username}, Id = {Id}", command.Username, command.Id);
         }
     }
 }
